058bc2e701ff4db10eb869236ef2e82cbd5e082b
rajivmoghe <rajivmoghe@yahoo.com>
2025-04-28 17:57:44 +0530

    minor update 2

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,6 +11,7 @@ function LoginPage({ onLoginSuccess }) {
   useEffect(() => {
     setTextboxValue("open sesame"); // Set default value after the page loads
 }, []);
+
   const handleLogin = () => {
     if (textboxValue.trim().toLowerCase() === "open sesame") {
       onLoginSuccess();
